e96af7fa2327610c0e3cf2401aee35057f636019
bala-amypo <bala@amypo.in>
2025-12-17 06:28:18 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/StudeServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudeServiceImpl.java
@@ -1,28 +1,5 @@
-package com.example.demo.services;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import org.springframework.stereotype.Service;
-import com.example.demo.entity.Studentity;
+package com.exmaple.demo.service.Impl;
+import java.util.*;
+import org.springframework.sterotype.Service;
+import com.example.demo.entity.Student;
 @Service
-public class StudeService {
-    private Map<Integer, Studentity> details = new HashMap<>();
-    public Studentity saveData(Studentity st) {
-        details.put(st.getId(), st);
-        return st;
-    }
-    public Studentity getById(int id) {
-        return details.get(id);
-    }
-    public Collection<Studentity> getAll() {
-        return details.values();
-    }
-    public Studentity updateStudent(int id, Studentity st) {
-        if(details.containsKey(id)) {
-            st.setId(id);
-            details.put(id, st);
-            return st;
-        }
-        return null;
-    }
-}
